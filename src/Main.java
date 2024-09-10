fa3fb41261c4af6e3496f67f06a95985d2de06cc
Chewbacca <higgimat000@warren.k12.in.us>
2024-09-10 13:40:56 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+    }
+
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+
+            // check if its even.  % 2
+            // if even add to sum.
+
+            if (lastDigit % 2 == 0) {
+                sum += lastDigit;
+            }
+
+            // number divide equals 10.
+            number /= 10;
+
+
+
+        }
+
+        return sum;
+
+
+    }
 }
